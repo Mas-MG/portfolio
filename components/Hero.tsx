bdafa1eca6e5553a01a83b8cffdd3cecaeced16e
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-04-15 15:22:22 +0330

    Add (Hero Section): grid bg by shadcn, spotlight by aceternity and next-themes by next.js

---
+++ components/Hero.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { cn } from "@/lib/utils";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className=" relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:80px_80px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+      <div className="flex justify-center relative">
+        <div className="max-w-[89vw]">
+          <h1
+            className="uppercase text-blue-100 tracking-widest
+          text-xs text-center max-w-80"
+          >
+            Dynamic Web Magic With Next.js
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
